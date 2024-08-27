05868b395146a3d2d219ae8f33ad5e49459d5ca5
张威 <zhangwei@192.168.0.102>
2024-08-27 21:53:23 +0800

    update

--- etc/docker/build.sh
+++ etc/docker/build.sh
@@ -4,9 +4,11 @@ set -e
 
 echo "1.切换到测试分支"
 # 切换到生产分支
-git switch master
+git switch main
 
 project_name="seaurl-drawio"
+project_port=19990
+project_namespace="drawio"
 
 # 定义aliyun 地址
 repository="registry.cn-hangzhou.aliyuncs.com/com-seaurl/${project_name}"
@@ -29,31 +31,32 @@ docker push "registry.cn-hangzhou.aliyuncs.com/com-seaurl/${project_name}:${comm
 
 echo "推送成功！"
 
-#echo "6.复制一份master环境的k8s yaml文件，目的是修改yaml文件并应用"
-## 复制一份新文件出来
-#cp k8s/seaurl-webapp.yaml k8s/webapp.yaml
-#
-#echo "7.修改上面的yaml文件"
-## 修改本地k8s yaml文件
-#perl -p -i -e "s#<domainName1>#seaurl.com#g;s#<domainName2>#www.seaurl.com#g;s#<imagename>#${repository}:${commit_hash}#g;s#<podname>#seaurl-webapp#g" k8s/webapp.yaml
-#
-#echo "8.应用上面的yaml文件"
-## 执行k8s命令应用上面修改的yaml文件
-#kubectl apply -f k8s/webapp.yaml -n seaurl
-#
-#echo "9.删除上面的yaml文件"
-##删除上面复制出来的文件
-#rm -rf k8s/webapp.yaml
-#
-#echo "10.删除上面编译的docker镜像"
-## 获取符合条件的镜像信息，并提取镜像 ID
-#image_ids=$(docker images | grep "${repository}" | awk '{print $3}')
-#
-## 循环删除每个镜像
-#for image_id in $image_ids; do
-#    echo "Image ID: ${image_id}"
-#    docker rmi $image_id
-#done
+echo "6.复制一份master环境的k8s yaml文件，目的是修改yaml文件并应用"
+# 复制一份新文件出来
+cp etc/k8s/${project_name}.yaml etc/k8s/new_${project_name}.yaml
+
+echo "7.修改上面的yaml文件"
+# 修改本地k8s yaml文件
+perl -p -i -e "s#<imagename>#${repository}:${commit_hash}#g;s#<podname>#${project_namespace}#g;s#<podport>#${project_port}#g" etc/k8s/new_${project_name}.yaml
+
+echo "8.应用上面的yaml文件"
+kubectl create namespace ${project_namespace}
+# 执行k8s命令应用上面修改的yaml文件
+kubectl apply -f etc/k8s/new_${project_name}.yaml -n ${project_namespace}
+
+echo "9.删除上面的yaml文件"
+#删除上面复制出来的文件
+rm -rf etc/k8s/new_${project_name}.yaml
+
+echo "10.删除上面编译的docker镜像"
+# 获取符合条件的镜像信息，并提取镜像 ID
+image_ids=$(docker images | grep "${repository}" | awk '{print $3}')
+
+# 循环删除每个镜像
+for image_id in $image_ids; do
+    echo "Image ID: ${image_id}"
+    docker rmi $image_id
+done
 
 echo "发布成功！"
 
